331079cc17d7cfe22de9aa8755d43ec7616972b9
michael sipayung <michaelsipayung123@gmail.com>
2021-05-20 13:02:20 +0700

    first commit

---
+++ build/CMakeFiles/associativeContainer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/associativeContainer.dir/main.cpp.obj"
+  "CMakeFiles/associativeContainer.dir/main.cpp.obj.d"
+  "associativeContainer.exe"
+  "associativeContainer.exe.manifest"
+  "associativeContainer.pdb"
+  "libassociativeContainer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/associativeContainer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <map>
+
+int main(int, char**) {
+    std::cout << "Hello, associative container!\n";
+    
+
+}
